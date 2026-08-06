cfab8b2c4628bf949dad341415ac3f4fc4f282e9
xlBlaCXlx <tonpalm255@gmail.com>
2026-08-06 21:38:38 +0700

    Test

--- favHero/src/data.js
+++
@@ -1,8 +0,0 @@
-export const HEROES = [
-    {id: '1', name: 'Tathagata Buddha', uri: 'https://static.wikia.nocookie.net/journey-to-the-west-xiyouji/images/c/c3/Tathagata_Buddha.jpeg/revision/latest?cb=20251026020432' },
-    {id: '2', name: 'Sun Wukong', uri: 'https://static.wikia.nocookie.net/journey-to-the-west-xiyouji/images/9/95/Sun_Wukong_Traditional.jpeg/revision/latest?cb=20251025080211' },
-    {id: '3', name: 'Tang Sanzang', uri: 'https://static.wikia.nocookie.net/journey-to-the-west-xiyouji/images/1/12/Tang_Sanzang_Traditional.jpeg/revision/latest?cb=20251025075530' },
-    {id: '4', name: 'Zhu Bajie', uri: 'https://static.wikia.nocookie.net/journey-to-the-west-xiyouji/images/8/8c/Zhu_Bajie_Traditional.jpeg/revision/latest?cb=20251025080359' },
-    {id: '5', name: 'Sha Wujing', uri: 'https://static.wikia.nocookie.net/journey-to-the-west-xiyouji/images/2/26/Sha_Wujing_Traditional.jpeg/revision/latest?cb=20251025080714' },
-    {id: '6', name: 'White Dragon Horse', uri: 'https://static.wikia.nocookie.net/journey-to-the-west-xiyouji/images/a/a1/White_Dragon_Spirit.jpeg/revision/latest?cb=20251026023440'},
-]
